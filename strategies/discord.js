c92e6ae54bf8d14425c60bb3871861a401859b02
petersonbabs <timiebabs@gmail.com>
2024-10-18 15:29:55 +0100

    added oauth

--- strategies/discord.js
+++ strategies/discord.js
@@ -7,7 +7,7 @@ export default passport.use(
     new Strategy({
         clientID: process.env.DISCORD_CLIENT_ID,
         clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        callbackURL: 'http://localhost:4000/api/v1/auth/third/discord/redirect',
+        callbackURL: ['http://localhost:4000/api/v1/auth/third/discord/redirect', 'https://barber-booking-backend-sw6w.onrender.com/api/v1/discord/redirect' ],
         scope: ['email', 'identify']
     },
         (accessToken, refreshToken, profile, done) =>{
